1840e6c19104243df36eb530a5540e95574bda95
boltgolt <boltgolt@gmail.com>
2018-06-07 11:07:47 +0200

    Inital game

---
+++ assets/ts/main.ts
@@ -0,0 +1,29 @@
+class Game {
+	public x:number = 0
+
+	constructor() {
+		console.log("d")
+		this.gameLoop()
+	}
+
+	private gameLoop(){
+		if (down) {
+			var current = parseInt(document.getElementById("thomas").style.left)
+			document.getElementById("thomas").style.left = current + 1 + "vh"
+		}
+
+
+		requestAnimationFrame(() => this.gameLoop())
+	}
+
+}
+
+window.addEventListener("load", () => new Game())
+
+let down = false
+window.addEventListener("keydown", (event) => {
+	if (event.key == "d") down = true
+})
+window.addEventListener("keyup", (event) => {
+	if (event.key == "d") down = false
+})
