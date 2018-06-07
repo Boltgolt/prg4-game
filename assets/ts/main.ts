c38293fc651d1edc535baa05f5c7c53d9b896934
boltgolt <boltgolt@gmail.com>
2018-06-07 23:09:33 +0200

    Added player classes with movement

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -1,28 +1,89 @@
+window.addEventListener("load", () => new Game())
+
 class Game {
-	public x:number = 0
+	private thomas:Thomas
+	private bob:Bob
+
+	private buttonLeft:Boolean = false
+	private buttonRight:Boolean = false
+	private buttonJump:Boolean = false
+
+
+	private active:any
+	private activeIndex:number = 0
+	private activeArr:Array<any> = []
 
 	constructor() {
-		console.log("d")
-		this.gameLoop()
+		this.thomas = new Thomas(widthToPixel(50) - heightToPixel(2.5))
+		this.bob = new Bob(widthToPixel(150) - heightToPixel(5))
+
+		this.active = this.thomas
+		this.activeArr.push(this.thomas)
+
+		window.addEventListener("keydown", this.handleKey.bind(this))
+		window.addEventListener("keyup", this.handleKey.bind(this))
+
+		this.update()
 	}
 
-	private gameLoop(){
-		if (down) {
-			var current = parseInt(document.getElementById("thomas").style.left)
-			document.getElementById("thomas").style.left = current + 1 + "vh"
+	private handleKey(event:KeyboardEvent) :void {
+		let setTo = event.type == "keydown"
+
+		switch(event.code) {
+			case "KeyA": {
+				this.buttonLeft = setTo
+				break
+			}
+			case "KeyD": {
+				this.buttonRight = setTo
+				break
+			}
+			case "KeyW": {
+				this.buttonJump = setTo
+				break
+			}
+			case "Space": {
+				if (setTo) break
+
+				if (this.activeIndex + 1 == this.activeArr.length) {
+					this.activeIndex = 0
+				}
+				else {
+					this.activeIndex++
+				}
+
+				this.active = this.activeArr[this.activeIndex]
+				break
+			}
+		}
+	}
+
+	private update() :void {
+		if (this.buttonLeft)  this.active.x -= this.active.speed
+		if (this.buttonRight) this.active.x += this.active.speed
+
+		if (this.activeArr.length == 1 && this.active.x > widthToPixel(100)) {
+			this.activeArr.push(this.bob)
+			document.getElementById("indicator").children[1].classList.remove("hidden")
 		}
 
+		this.active.update()
 
-		requestAnimationFrame(() => this.gameLoop())
+		this.focusCamera()
+
+
+		requestAnimationFrame(() => this.update())
+	}
+
+	private focusCamera() :void {
+		window.scrollTo(this.active.x - widthToPixel(50) + heightToPixel(2.5 * this.active.width), 0)
 	}
 }
 
-window.addEventListener("load", () => new Game())
+function widthToPixel(width:number) :number {
+	return window.innerWidth * (width / 100)
+}
 
-let down = false
-window.addEventListener("keydown", (event) => {
-	if (event.key == "d") down = true
-})
-window.addEventListener("keyup", (event) => {
-	if (event.key == "d") down = false
-})
+function heightToPixel(height:number) :number {
+	return window.innerHeight * (height / 100)
+}
--- assets/ts/player.ts
+++ assets/ts/player.ts
@@ -1,5 +1,37 @@
-class Player {
-    constructor() {
-		
+class PlayerObject {
+    private DOMNode:HTMLElement
+
+    public x:number
+    public y:number
+    public speed:number = 5
+
+    constructor(x:number, id:string) {
+        this.x = x
+        this.DOMNode = document.getElementById(id)
+
+        document.getElementById(id).style.left = x + "px"
+	}
+
+    update() {
+        this.DOMNode.style.left = this.x + "px"
+
+    }
+}
+
+class Thomas extends PlayerObject {
+    public height:number = 1
+    public width:number = 1
+
+    constructor(x:number) {
+        super(x, "thomas")
+	}
+}
+
+class Bob extends PlayerObject {
+    public height:number = 2
+    public width:number = 2
+
+    constructor(x:number) {
+        super(x, "bob")
 	}
 }
